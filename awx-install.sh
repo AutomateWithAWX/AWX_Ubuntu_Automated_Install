8d4fdd2a869a3bc3c06ce71cb60ad38a82efe74d
Chase Ballard <Chaserayball@gmail.com>
2021-09-03 23:27:38 -0500

    Create awx-install.sh
    awx-install

---
+++ awx-install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail
+sudo apt update
+sudo apt-get install -y software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu\
+ $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+sudo systemctl restart docker
+sudo docker version
+curl -s https://api.github.com/repos/docker/compose/releases/latest \
+  | grep browser_download_url | grep docker-compose-Linux-x86_64 \
+  | cut -d '"' -f 4 | wget -qi -
+sudo chmod +x docker-compose-Linux-x86_64
+sudo mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+sudo docker-compose version
+sudo apt install -y ansible
+ansible --version
+sudo apt install -y nodejs npm
+sudo npm install npm --global
+sudo apt install -y python3-pip git pwgen
+sudo pip3 install docker-compose==1.28.5
+wget https://github.com/ansible/awx/archive/15.0.0.zip
+unzip -o 15.0.0.zip
+touch ~/awxsecret
+cd awx-15.0.0/installer/
+$awxsecret='pwgen -N 1 -s 30'
+sed -i 's/# admin_password=password/admin_password=password/' ./inventory
+sed -i 's/secret_key=awxsecret/secret_key=$awxsecret/' ./inventory
+ansible-playbook -i inventory install.yml
+sudo systemctl restart docker
+x-www-browser 'http://localhost'
